a84d96273c78f4ff96cb4f1a0c9c359ca6718279
fab-ryan <royalfabrice1234@gmail>
2022-04-11 16:20:06 +0200

    final configuration

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Hello wolrd</h1>
+      <h3>Dominators frontend</h3>
+    </div>
+  );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import reactDom from "react-dom/client";
+import App from "./app";
+const root = reactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
