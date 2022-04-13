70d3e020a2c3fb7bf92ef91b21f5e8c2a2220b73
fab-ryan <royalfabrice1234@gmail>
2022-04-13 17:19:27 +0200

    How does it work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,31 @@
 import UserNavbar from "../components/Navbars/UserNavbar";
 import UserHeader from "../components/Headers/UserHeader";
+import { IoLocationSharp } from "react-icons/io5";
+import { HiClock } from "react-icons/hi";
+import { BsKeyboardFill } from "react-icons/bs";
 function Home() {
   return (
     <div>
       <UserNavbar />
       <UserHeader />
       <div className="home-content">
-        <h3>
-          HOW DOES IT WORKER
-        </h3>
-        <div>
-          <div className="col-3">Register</div>
-          <div className="col-3"></div>
-          <div className="col-3"></div>
+        <h3>HOW DOES IT WORKER</h3>
+        <div className="row">
+          <div className="col-3">
+            <BsKeyboardFill className="icons" />
+            REGISTER
+            <div className="home-circle">1</div>
+          </div>
+          <div className="col-3">
+            <IoLocationSharp className="icons" />
+            TRACK YOU BUS
+            <div className="home-circle">2</div>
+          </div>
+          <div className="col-3">
+            <HiClock className="icons" />
+            TRAVEL ON TIME
+            <div className="home-circle">3</div>
+          </div>
         </div>
       </div>
     </div>
