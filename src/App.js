cf7daa4b63953759c574a25e1ce4b60ffeade6b2
gerard1001 <rutayisiregerard2002@gmail.com>
2022-04-14 12:34:41 +0200

    added logout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+
 import UpdateProfile from "./components/update-profile/UpdateProfile";
+
+// import axios from "axios";
+
+// import axios from "axios";
 import Home from "./pages/Home/Home";
 import PageNotFound from "./pages/PageNotFound";
 import Logout from "./pages/Logout";
 import Login from "./pages/Login";
-
-
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} exact={true} />
       <Route path="/update-profile" element={<UpdateProfile />} exact={true} />
       <Route path="/*" element={<PageNotFound />} />
-      <Route path="/logout" element={<Logout />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   );
