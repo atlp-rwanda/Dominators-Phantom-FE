57e29ccc4df07d6722e29b0cf79bbddbfd70a204
gerard1001 <rutayisiregerard2002@gmail.com>
2022-04-14 13:08:10 +0200

    home page/ page not found

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import PageNotFound from "./pages/PageNotFound";
-import Logout from "./components/Logout/Logout";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} exact={true} />
       <Route path="/*" element={<PageNotFound />} />
-      <Route path="/logout" element={<Logout />} />
     </Routes>
   );
 }
