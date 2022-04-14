8cf81a4284e7a7a24a04e5b470fe6984018f988b
gerard1001 <rutayisiregerard2002@gmail.com>
2022-04-14 12:34:41 +0200

    added logout

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import reactDom from "react-dom/client";
 import App from "./app";
 import { BrowserRouter } from "react-router-dom";
 import "./style.css";
-<<<<<<< HEAD
-import "./css/Login.css";
-=======
-import { createStore } from "redux"; //
-import allReducers from "./redux/reducers"; //
-import { Provider } from "react-redux"; //
->>>>>>> 811718ce6604b6077be18b08accc04c4dfa61bb6
+
+// import "./css/Login.css";
+
+import { createStore } from "redux"; 
+import allReducers from "./redux/redux/counterreducer"; 
+import { Provider } from "react-redux"; 
 
 const root = reactDom.createRoot(document.getElementById("root"));
 
