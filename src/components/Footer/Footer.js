3a5d9f6f746af0ea6f7546e63d118ed120d997a7
fab-ryan <royalfabrice1234@gmail>
2022-04-16 12:08:40 +0200

    responsive nav bar

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,68 @@
 import "./footer.css";
+import { NavLink } from "react-router-dom";
+import {
+  AiFillFacebook,
+  AiFillTwitterSquare,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer-limit">
+        <div className="footer-content">
+          <div className="footer-quick-links">
+            QUICK LINKS
+            <ul>
+              <li>
+                <NavLink to="/ticket">&gt;TICKET</NavLink>
+              </li>
+              <li>
+                <NavLink to="/live-tracking">&gt;LIVE TRACKING</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">&gt;CONTACT</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-contact-us">
+            CONTACT US
+            <ul>
+              <li>kk 767 street</li>
+              <li>Kigali Rwanda</li>
+              <li>+2507888888</li>
+              <li>phantom@gmail.com</li>
+            </ul>
+          </div>
+          <div className="footer-social-media">
+            SOCIAL MEDIA
+            <ul>
+              <li>
+                <AiFillFacebook className="icon-facebook" />
+                &nbsp; Facebook
+              </li>
+              <li>
+                <AiFillTwitterSquare className="icon-twitter" />
+                &nbsp; Twitter
+              </li>
+              <li>
+                <AiFillLinkedin className="icon-linkedin" />
+                &nbsp; LinkedIn
+              </li>
+              <li>
+                <AiFillYoutube className="icon-youtube" />
+                &nbsp; Youtube
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="hr" />
+        <div className="designer">
+          <p className="p1">Phantom 2022. All rights reserved</p>
+          <p className="p2">Designed by ATLP Dominators</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Footer;
--- src/components/Navbars/UserNavbar.js
+++ src/components/Navbars/UserNavbar.js
@@ -1,33 +1,80 @@
 import phantom from "../../assets/images/logos/phantomIcon.png";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import "./UserNavbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
 function UserNavbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  let humbuger;
+  const toggleNav = () => {
+    if (toggleMenu === false) {
+      setToggleMenu(true);
+    } else {
+      setToggleMenu(false);
+    }
+  };
+
   return (
     <div className="navBar">
       <nav className="nav">
         <div className="logo">
           <img alt="" src={phantom} width="120px" height="65px" />
         </div>
-        <div className="navItem">
-          <ul>
-            <li>
-              <NavLink to="/">HOME</NavLink>
-            </li>
-            <li>
-              <NavLink to="ticket">TICKET</NavLink>
-            </li>
-            <li>
-              <NavLink to="tracking">LIVE TRACKING</NavLink>
-            </li>
-            <li>
-              <NavLink to="contact">CONTACT</NavLink>
-            </li>
-            <li>
-              <NavLink to="login" className="login">
-                LOGIN
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+        {toggleMenu ? (
+          <div className="responsiveNavItem">
+            <ImCross
+              className="close"
+              onClick={() => {
+                setToggleMenu( false );
+                humbuger={$}
+              }}
+            />
+            <ul>
+              <li>
+                <NavLink to="/">HOME</NavLink>
+              </li>
+              <li>
+                <NavLink to="ticket">TICKET</NavLink>
+              </li>
+              <li>
+                <NavLink to="tracking">LIVE TRACKING</NavLink>
+              </li>
+              <li>
+                <NavLink to="contact">CONTACT</NavLink>
+              </li>
+              <li>
+                <NavLink to="login" className="login">
+                  LOGIN
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div className="navItem">
+            <ul>
+              <li>
+                <NavLink to="/">HOME</NavLink>
+              </li>
+              <li>
+                <NavLink to="ticket">TICKET</NavLink>
+              </li>
+              <li>
+                <NavLink to="tracking">LIVE TRACKING</NavLink>
+              </li>
+              <li>
+                <NavLink to="contact">CONTACT</NavLink>
+              </li>
+              <li>
+                <NavLink to="login" className="login">
+                  LOGIN
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        <GiHamburgerMenu className="hamburger" onClick={toggleNav} />
       </nav>
     </div>
   );
