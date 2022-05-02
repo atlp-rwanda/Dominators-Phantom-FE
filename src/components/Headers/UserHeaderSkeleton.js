db3819603a8c7b7dbeced6d299f0c3f5a55df971
aimelive <aimendayambaje24@gmail.com>
2022-05-02 10:20:49 +0200

    heroku deploy

--- src/components/Headers/UserHeaderSkeleton.js
+++ src/components/Headers/UserHeaderSkeleton.js
@@ -1,13 +1,13 @@
-import SkeletonElement from "../../skeletons/skeletonElement";
+import SkeletonElement from "../../skeletons/SkeletonElement";
+
 function UserHeaderSkeleton() {
   return (
     <>
       <div className="skeleton-wrapper">
         <div className="skeleton-userheader">
-
           <div className="skeleton-descr">
-            <SkeletonElement type="title" height={ 20 } width={ 200 } />
-            <SkeletonElement type="title" height={ 50 } width={ 120}/>
+            <SkeletonElement type="title" height={20} width={200} />
+            <SkeletonElement type="title" height={50} width={120} />
             <SkeletonElement type="text" />
             <SkeletonElement type="text" />
             <SkeletonElement type="text" />
@@ -19,8 +19,6 @@ function UserHeaderSkeleton() {
             <SkeletonElement type="thumbnail" height={300} width={300} />
           </div>
         </div>
-
-
       </div>
     </>
   );
