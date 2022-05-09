b5ef2d8b2397e37f231554f49f26a7d5fc3604cd
GilbertUp <98816131+GilbertUp@users.noreply.github.com>
2022-05-09 13:55:01 +0200

    adjust

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,11 +1,4 @@
 import React, { Component } from 'react'
-import groupIcon from '../images/phantom.png'
-import dash from '../images/images.png'
-import user from '../images/user.png'
-import driver from '../images/driver1.png'
-import setting from '../images/setting.png'
-import notify from '../images/notify.png'
-import logout from '../images/logout.png'
 import * as FaIcons from "react-icons/fa"
 
 
