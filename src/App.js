ce52a9e8f54a7e20ebcb063c53682432a26c9f24
UmuhireJessie <j.umuhire@alustudent.com>
2022-05-14 18:07:07 +0200

    rebased to develop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import UpdateProfile from "./components/update-profile/UpdateProfile";
+import Login from "./pages/Login";
 import Home from "./pages/Home/Home";
 import PageNotFound from "./pages/PageNotFound";
 import "react-toastify/dist/ReactToastify.css";
