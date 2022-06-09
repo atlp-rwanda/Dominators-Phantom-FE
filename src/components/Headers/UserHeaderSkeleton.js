02288637992286368e3848580ac70182666d6f20
tjeaneric <ericjohn415@gmail.com>
2022-06-09 22:24:28 +0200

    Resolved start script as per #26

--- src/components/Headers/UserHeaderSkeleton.js
+++ src/components/Headers/UserHeaderSkeleton.js
@@ -1,12 +1,12 @@
-import SkeletonElement from "../../skeletons/skeletonElement";
+import SkeletonElement from "../../skeletons/SkeletonElement";
 function UserHeaderSkeleton() {
   return (
     <>
       <div className="skeleton-wrapper">
         <div className="skeleton-userheader">
           <div className="skeleton-descr">
-            <SkeletonElement type="title" height={ 20 } width={ 200 } />
-            <SkeletonElement type="title" height={ 50 } width={ 120}/>
+            <SkeletonElement type="title" height={20} width={200} />
+            <SkeletonElement type="title" height={50} width={120} />
             <SkeletonElement type="text" />
             <SkeletonElement type="text" />
             <SkeletonElement type="text" />
